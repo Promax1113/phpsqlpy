8c013d465f76757c6fdf2ad1c90c4d1036476f37
promax1113 <aatenorio.estruch@gmail.com>
2023-01-28 21:08:24 +0000

    commit

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
-import randint
+import random
 
+def storyName(name):
+    story = ["happy", "sad", "excited", "tired", "angry", "hungry", "brave", "calm", "charming", "clever", "cool", "courageous", "creative", "determined", "elegant", "energetic", "enthusiastic", "friendly", "funny", "generous", "gentle", "glorious", "good", "great", "handsome", "honest", "humorous", "important", "interesting", "kind", "loving", "lucky", "nice", "optimistic", "proud", "relaxed", "reliable", "sincere", "smart", "successful", "thoughtful", "understanding", "vibrant", "warm", "witty", "wonderful", "zealous"]
+    return story[random.randint(1, len(story))].capitalize() + " " + name
 
-def create_story_name(name):
-    story = ["mighty", "desperate", "giant"]
-
-
-    return story[random.randint(0, story.len())] + name
-
-print(create_story_name(Albert))
+print(storyName("Gerald"))
